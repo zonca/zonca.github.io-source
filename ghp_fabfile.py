bcda62521ff13128e484bb0d4c434ef2cff78c3a
Andrea Zonca <code@andreazonca.com>
2013-09-26 13:20:25 -0700

    fabfile now obsolete

--- ghp_fabfile.py
+++ ghp_fabfile.py
@@ -1,15 +1,16 @@
 from fabric.api import *
 from fabfile import DEPLOY_PATH
 
+
 REMOTE_BRANCH = "master"
-REMOTE_URL = "git@github.com:zonca/zonca.github.io.git"
+REMOTE_URL = "https:// @github.com:zonca/zonca.github.io.git"
 COMMIT_MESSAGE = "deploy"
 
 def init():
     local('rm -r {0}'.format(DEPLOY_PATH))
     local('mkdir {0}'.format(DEPLOY_PATH))
     with lcd(DEPLOY_PATH):
-        local('git clone -b {remote_branch} {remote} .'.format(remote_branch=REMOTE_BRANCH, remote=REMOTE_URL))
+        local('git clone --quiet -b {remote_branch} {remote} .'.format(remote_branch=REMOTE_BRANCH, remote=REMOTE_URL))
 
 def deploy():
     with lcd('output'):
