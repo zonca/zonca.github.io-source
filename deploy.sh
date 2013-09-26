4c4134519b59a2fee066ff8991857ca2ad8c0a1f
Andrea Zonca <code@andreazonca.com>
2013-09-26 16:28:49 -0700

    support for local deploy

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,10 @@ PELICAN_OUTPUT_FOLDER=output
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo -e "Starting to deploy to Github Pages\n"
-    git config --global user.email "travis@travis-ci.org"
-    git config --global user.name "Travis"
+    if [ "$TRAVIS" == "true" ]; then
+        git config --global user.email "travis@travis-ci.org"
+        git config --global user.name "Travis"
+    fi
     #using token clone gh-pages branch
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
     #go into directory and copy data we're interested in to that directory
