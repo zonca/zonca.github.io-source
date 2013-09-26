e79204ffe00586d6192f2c0115ec22e8e163c019
Andrea Zonca <code@andreazonca.com>
2013-09-26 13:40:26 -0700

    typo

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,11 @@ PELICAN_OUTPUT_FOLDER=output
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo -e "Starting to deploy to Github Pages\n"
-    #go to home and setup git
-    #cd $HOME
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis"
     #using token clone gh-pages branch
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
-    #go into diractory and copy data we're interested in to that directory
+    #go into directory and copy data we're interested in to that directory
     cd build_website
     cp -Rf ../$PELICAN_OUTPUT_FOLDER/* .
     #add, commit and push files
