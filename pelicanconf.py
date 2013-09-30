db6a962daefcdf6ed2f113ca64af508c33655e16
Andrea Zonca <code@andreazonca.com>
2013-09-30 09:29:43 -0700

    fixed about.me username

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,7 +34,7 @@ THEME = "./middle-theme/middle"
 
 TWITTER_USERNAME = "andreazonca"
 GITHUB_USERNAME = "zonca"
-ABOUTME_USERNAME = "zonca"
+ABOUTME_USERNAME = "andreazonca"
 GOOGLEPLUS_USERNAME = "102215770805040369170"
 EMAIL_ADDRESS = "andrea.zonca|on the google mail service"
 HOMEPAGE_INTRO = "Andrea Zonca: notes about Python, high performance computing, data analysis"
