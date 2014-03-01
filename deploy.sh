f22255affef70a67e7c938deabc662d4ae6168b0
Andrea Zonca <code@andreazonca.com>
2014-02-28 17:42:19 -0800

    test travis-encrypt

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@ BRANCH=master
 TARGET_REPO=zonca/zonca.github.io.git
 PELICAN_OUTPUT_FOLDER=output
 
+echo -e "Testing travis-encrypt"
+echo -e "$VARNAME"
+
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo -e "Starting to deploy to Github Pages\n"
     if [ "$TRAVIS" == "true" ]; then
